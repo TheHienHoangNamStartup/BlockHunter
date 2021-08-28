63404bac627fef12fb85b60920fbbaa2adde2b59
Nam <62420404+HoangNamDo@users.noreply.github.com>
2021-08-28 00:08:08 -0700

    Temp 2

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,1 @@
-const CELL = 2; // CELL * 1rem | CELL * 16px
-const CONTAINER_WIDTH = 12;
-const CONTAINER_HEIGHT = 16;
-const COLOR = {
-  red: "#e74c3c",
-  orange: "#e67e22",
-  yellow: "#f1c40f",
-  blue: "#3498db",
-  green: "#1abc9c",
-  purple: "#9b59b6",
-};
+const WEAPON_SIZE = 1;
