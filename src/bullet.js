8c5a5f9fad37a9249588ac3d72c0d347f085a745
Yam <phamthehien0410@gmail.com>
2021-08-28 14:52:14 +0700

    Class bullet

---
+++ src/bullet.js
@@ -0,0 +1,13 @@
+class Bullet {
+  constructor(color) {
+    this.color = color;
+  }
+
+  draw() {
+    let bullet = document.createElement("div");
+    bullet.style.width = `${CELL}rem`;
+    bullet.style.height = `${CELL}rem`;
+    bullet.style.backgroundColor = this.color;
+    document.getElementById("weapon").appendChild(bullet);
+  }
+}
